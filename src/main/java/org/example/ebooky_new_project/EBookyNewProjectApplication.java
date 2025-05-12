de606862017b9ac68ad015551bea930e610d8fa5
IT24103206 <it24103206@my.sliit.lk>
2025-05-12 10:50:24 +0530

    Initial commit

---
+++ src/main/java/org/example/ebooky_new_project/EBookyNewProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.ebooky_new_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookyNewProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBookyNewProjectApplication.class, args);
+    }
+
+}
