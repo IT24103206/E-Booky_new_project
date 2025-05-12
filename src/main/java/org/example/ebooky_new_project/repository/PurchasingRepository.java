cce5252189b66173e107b9afa5d20d6bb01a0bf1
IT24102935 <it24102935@my.sliit.lk>
2025-05-12 23:49:08 +0530

    PurchasingRepositoryImpl add

--- src/main/java/org/example/ebooky_new_project/repository/PurchasingRepository.java
+++ src/main/java/org/example/ebooky_new_project/repository/PurchasingRepository.java
@@ -1,4 +1,12 @@
 package org.example.ebooky_new_project.repository;
 
+import org.example.ebooky_new_project.model.Purchasing;
+
+import java.util.List;
+
 public interface PurchasingRepository {
+    List<Purchasing> getAllPurchasing();
+    Purchasing addPurchasing(Purchasing purchasing);
+    Purchasing updatePurchasing(Purchasing purchasing);
+    boolean deletePurchasing(int id);
 }
