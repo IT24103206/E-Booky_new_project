e4c2a43682460e6df10b2c594a740ff7a03591df
Dinali <it24102922@my.sliit.lk>
2025-05-13 20:40:56 +0530

    Feedback controller add

--- src/main/java/org/example/ebooky_new_project/controllers/FeedbackController.java
+++ src/main/java/org/example/ebooky_new_project/controllers/FeedbackController.java
@@ -1,4 +1,47 @@
 package org.example.ebooky_new_project.controllers;
 
+import org.example.ebooky_new_project.model.Book;
+import org.example.ebooky_new_project.model.Feedback;
+import org.example.ebooky_new_project.model.RegularUser;
+import org.example.ebooky_new_project.model.User;
+import org.example.ebooky_new_project.service.FeedbackService;
+import org.example.ebooky_new_project.service.FeedbackServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
 public class FeedbackController {
+    private final FeedbackService service;
+    public FeedbackController(){
+        this.service = new FeedbackServiceImpl();
+    }
+
+    @GetMapping("/feedback")
+    public List<Feedback> getAllFeedback(){
+        return service.getAllFeedback();
+    }
+
+    @GetMapping("/feedback/user/{id}")
+    public List<Feedback> getUserFeedback(@PathVariable int id){
+        User user = new RegularUser();
+        user.setUserId(id);
+
+        return service.getAllFeedback(user);
+    }
+
+    @GetMapping("/feedback/book/{id}")
+    public List<Feedback> getBookFeedback(@PathVariable int id){
+        Book book = new Book();
+        book.setBookId(id);
+
+        return service.getAllFeedback(book);
+    }
+
+    @PostMapping("/feedback")
+    public Optional<Feedback> addFeedback(@RequestBody Feedback feedback){
+        return service.addFeedback(feedback);
+    }
 }
