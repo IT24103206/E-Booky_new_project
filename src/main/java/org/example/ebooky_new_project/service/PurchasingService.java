69af0cf24715acd0749e9ebd426ccc3ad7d0953e
IT24102935 <it24102935@my.sliit.lk>
2025-05-13 17:05:06 +0530

    PurchasingServiceImpl add

--- src/main/java/org/example/ebooky_new_project/service/PurchasingService.java
+++ src/main/java/org/example/ebooky_new_project/service/PurchasingService.java
@@ -1,4 +1,17 @@
 package org.example.ebooky_new_project.service;
 
+import org.example.ebooky_new_project.model.Purchasing;
+import org.example.ebooky_new_project.model.User;
+
+import java.awt.print.Book;
+import java.util.List;
+import java.util.Optional;
+
 public interface PurchasingService {
+    List<Purchasing> getAllPurchasing();
+    List<Purchasing> getAllPurchasing(User user);
+    List<Purchasing> getAllPurchasing(Book book);
+
+    Optional<Purchasing> addPurchasing(Purchasing purchasing);
+    Optional<Purchasing> updatePurchasing(Purchasing purchasing);
 }
---
+++ src/main/java/org/example/ebooky_new_project/service/PurchasingServiceImpl.java
@@ -0,0 +1,4 @@
+package org.example.ebooky_new_project.service;
+
+public class PurchasingServiceImpl {
+}
