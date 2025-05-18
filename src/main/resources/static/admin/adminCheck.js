40b69871467fd6279cd87487d191c754128f304a
IT24103206 <it24103206@my.sliit.lk>
2025-05-18 17:52:21 +0530

    adminCheck.js file add

---
+++ src/main/resources/static/admin/adminCheck.js
@@ -0,0 +1,13 @@
+function adminCheck() {
+    const user = JSON.parse(localStorage.getItem("currentUser"));
+    if(!user){
+        window.location.href = "./login.html";
+        return;
+    }
+
+    if(user["@type"]!="AdminUser"){
+        window.location.href = "./login.html";
+        return;
+    }
+}
+adminCheck();
